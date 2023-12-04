2b25b6f86e91734aabc0d40bafb7b10a1e2c92d9
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2023-12-04 09:27:43 -0600

    Se han añadido los archivos app.py, Procfile, requirements.txt y runtime.txt

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/buscar", methods=["GET"])
+def search_contact():
+    return render_template('buscar.html')
+
+@app.route("/insertar", methods=["GET", "POST"])
+def create_contact():
+    return render_template('insertar.html')
+
+@app.route("/ver", methods=["GET"])
+def details():
+    return render_template('ver.html')
+
+@app.route("/borrar", methods=["GET", "POST"])
+def delete_contact():
+    return render_template('borrar.html')
+
+@app.route("/editar", methods=["GET", "POST"])
+def update_contact():
+    return render_template('editar.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
