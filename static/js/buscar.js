34ea158f6de04a77398c86721a995a81314e186c
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2023-12-06 01:13:40 -0600

    Se ha cambiado la estructura del código buscar.js

--- static/js/buscar.js
+++ static/js/buscar.js
@@ -1,29 +1,23 @@
 const API_URL = "https://contactos-api-backend-heroku-70960492bc44.herokuapp.com/";
-
 const xhr = new XMLHttpRequest();
 
 function buscar() {
-    const email  = document.getElementById('email').value;
-    xhr.open ("GET", `${API_URL}?email=${email}`);
+    const email = document.getElementById('email').value;
+    xhr.open("GET", `${API_URL}?email=${encodeURIComponent(email)}`);
     xhr.send();
 }
 
 function onRequestHandler() {
-    if (this.readyState == 4 && this.status == 200) {
-        // 0 = UNSET, no se ha llamado al método open
-        // 1 = OPENED, se ha llamado al método open
-        // 2 = HEADERS_RECEIVED, se está llamando al método send()
-        // 3 = LOADING, está cargando, es decir, está recibiendo la respuesta.
-        // 4 = DONE, se ha completado la operación.
+    if (this.readyState === 4) {
+        if (this.status === 200) {
+            const data = JSON.parse(this.response);
+            const HTMLResponse = document.getElementById("datos");
 
-        
-        const data = JSON.parse(this.response);
-        const HTMLResponse = document.querySelector("#datos");
-
-        const tpl = data.map((user) => `<tr><td>${user.email}</td><td>${user.nombre}</td><td>${user.telefono}</td><td><a href="ver.html" class="opciones">ver</a></td><td><a href="editar.html" class="opciones">editar</a></td><td><a href="borrar.html" class="opciones">borrar</a></td></tr>`);
-        HTMLResponse.innerHTML = tpl.join(''); // Asigna nuevo contenido en el elemento HTMLResponse
-
-        // HTMLResponse.insertAdjacentHTML('beforeend', tpl.join(''));  // "Añade" nuevo contenido al elemento HTMLResponse
+            const tpl = data.map((user) => `<tr><td>${user.email}</td><td>${user.nombre}</td><td>${user.telefono}</td><td><a href="ver.html" class="opciones">ver</a></td><td><a href="editar.html" class="opciones">editar</a></td><td><a href="borrar.html" class="opciones">borrar</a></td></tr>`);
+            HTMLResponse.innerHTML = tpl.join('');
+        } else {
+            console.error("Error en la solicitud:", this.status, this.statusText);
+        }
     }
 }
 
@@ -32,9 +26,8 @@ document.getElementById('buscarForm').addEventListener('submit', function (event
     buscar();
 });
 
-xhr.addEventListener("load", onRequestHandler);
-
-
 function cancelar() {
     window.location.href = 'index.html';
 }
+
+xhr.addEventListener("load", onRequestHandler);
