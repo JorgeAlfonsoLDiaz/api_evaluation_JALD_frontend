723513e08fd429234367c6d71ebbcae6af4efa6d
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2023-12-13 10:26:33 -0600

    Se implementó funcionalidad en signup.html

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,34 +1,38 @@
-function registerUser() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-  
-    var data = {
-      username: username,
-      password: password
-    };
-  
-    var request = new XMLHttpRequest();
-    request.open('POST', 'https://contactos-api-backend-heroku-70960492bc44.herokuapp.com/registrar', true);
-    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-  
-    var encodedCredentials = btoa(username + ':' + password);
-    request.setRequestHeader('Authorization', 'Basic ' + encodedCredentials);
-  
-    request.onload = function () {
-      if (request.status >= 200 && request.status < 400) {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('insertarForm').addEventListener('submit', function (event) {
 
-        alert("Registro exitoso");
+        function registerUser() {
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
         
-        window.location.href = '/login';
-      } else {
-        console.error("Error al registrarse");
-      }
-    };
-  
-    request.onerror = function () {
-      console.error("Error de conexión");
-    };
-  
-    request.send(JSON.stringify(data));
-  }
-  
+            var data = {
+            username: username,
+            password: password
+            };
+        
+            var request = new XMLHttpRequest();
+            request.open('POST', 'https://contactos-api-backend-heroku-70960492bc44.herokuapp.com/registrar', true);
+            request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        
+            var encodedCredentials = btoa(username + ':' + password);
+            request.setRequestHeader('Authorization', 'Basic ' + encodedCredentials);
+        
+            request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+
+                alert("Registro exitoso");
+                
+                window.location.href = '/login';
+            } else {
+                console.error("Error al registrarse");
+            }
+            };
+        
+            request.onerror = function () {
+            console.error("Error de conexión");
+            };
+        
+            request.send(JSON.stringify(data));
+        }
+    });
+});
